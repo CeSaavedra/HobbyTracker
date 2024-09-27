ceee6f6fc61c878c80d8cbf7a8c8bf8fc7c72ac4
Cesar Saavedra <129910837+CeSaavedra@users.noreply.github.com>
2024-09-27 10:45:51 -0400

    Initial Commit

---
+++ HobbyTracker/HobbyTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HobbyTrackerApp.swift
+//  HobbyTracker
+//
+//  Created by Cesar S on 9/27/24.
+//
+
+import SwiftUI
+
+@main
+struct HobbyTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
